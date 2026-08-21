4d328e8880e581f242f48c3208ddad6d57b5a18e
Milan Kumar <148351992+milan27k@users.noreply.github.com>
2026-08-21 17:27:42 +0530

    Delete database.py

--- database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-database_url = "postgresql://postgres:123456789@localhost:5432/ai_sales_agent"
-
-engine = create_engine(database_url)
-sessionLocal = sessionmaker(autoflush=False,bind=engine,autocommit = False)
