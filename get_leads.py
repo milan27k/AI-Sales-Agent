cb4e43e96a766726e0a3e771dbb00177b70b2ebf
Milan Kumar <148351992+milan27k@users.noreply.github.com>
2026-08-21 17:28:21 +0530

    Delete get_leads.py

--- get_leads.py
+++
@@ -1,8 +0,0 @@
-from database import sessionLocal
-from database_model import Lead
-
-def get_all_leads():
-    db = sessionLocal()
-    leads = db.query(Lead).all()
-    db.close()
-    return leads
